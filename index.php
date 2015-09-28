64563e62f9877752883f8a4dec3d5338dd1fee09
Jorge Gil <jorgegilcavazos@gmail.com>
2015-09-27 21:22:47 -0500

    Fix index.php season selection

--- index.php
+++ index.php
@@ -1,14 +1,39 @@
 <?php
 include ("res/TeamNames.php");
 
-$season = $_GET['season'];
-if ($season < 1950) {
-	//$file = fopen("data/leagues_BAA_".$season."_games_games.csv", "r");
+$json_string = file_get_contents("res/season-holders.json");
+$json_data = json_decode($json_string, true);
+
+if(isset($_GET['season'])) {
+	$season = $_GET['season'];
 } else {
-	//$file = fopen("data/leagues_NBA_".$season."_games_games.csv", "r");
+	$season = 2015;
 }
 
-$holder = "New York Knicks";
+if ($season == "all") {
+	$all = true;
+	$season = 1947;
+}
+else {
+	$all = false;
+}
+
+
+if ($season < 1950) {
+	$league = "BAA";
+} else {
+	$league = "NBA";
+}
+
+
+// Gets last holder of last season
+foreach ($json_data as $key => $row) {
+	if ($row['season'] == ($season - 1) && $row['type'] == "playoffs") {
+	    $holder = $json_data[$key]['holder'];
+	}
+}
+
+
 $cont = 0;
 ?>
 
@@ -34,11 +59,17 @@ $cont = 0;
 			<div class="form-group">
 			<label>Season Game Log:</label>
 			<select class="form-control" id="season" name="" onchange="javascript:location.href = this.value;">
-				<option value="?season=1947" <?php if ($season == "1947") {?> selected <?php } ?> >1946/47</option>
-				<option value="?season=1948" <?php if ($season == "1948") {?> selected <?php } ?> >1947/48</option>
-				<option value="?season=1949" <?php if ($season == "1949") {?> selected <?php } ?> >1948/49</option>
-				<option value="?season=1950" <?php if ($season == "1950") {?> selected <?php } ?> >1949/50</option>
-				<option value="?season=1951" <?php if ($season == "1951") {?> selected <?php } ?> >1950/51</option>
+				<option value="?season=all" <?php if ($all) {?> selected <?php } ?> >All Seasons</option>
+				<?php
+				$num = 2015;
+				while ($num > 1947) {
+					?>
+				<option value="?season=<?php echo $num; ?>" <?php if ($season == $num) {?> selected <?php } ?> ><?php echo ($num - 1)."/".$num; ?></option>
+				<?php
+					$num--;
+				}
+				?>
+				<option value="?season=1947" <?php if ($season == "1947" && !$all) {?> selected <?php } ?> >1946/1947</option>
 			</select>
 			</div>
 			</div>
@@ -58,8 +89,6 @@ $cont = 0;
 				<tbody>
 				<?php
 				
-				$league = "BAA";
-				$season = 1947;
 				$streak = 0;
 				$largest_streak = 0;
 				$mode = 0;
@@ -68,6 +97,7 @@ $cont = 0;
 					if ($mode == 0) {
 						$file = fopen("data/leagues_".$league."_".$season."_games_games.csv", "r");
 						$mode = 1;
+						$season_streak = 0;
 					}else {
 						$file = fopen("data/leagues_".$league."_".$season."_games_games_playoffs.csv", "r");
 						$mode = 0;
@@ -79,15 +109,22 @@ $cont = 0;
 								$temp = $holder;
 								if ($line[3] > $line[5]) {
 									$holder = $line[2];
+									$losser = $line[4];
 								}
 								else {
 									$holder = $line[4];
+									$losser = $line[2];
 								}
 								if ($temp == $holder) {
 									$streak++;
+									$season_streak++;
 								} else {
-									$streak = 1;;
+									$streak = 1;
+									$season_streak = 1;
 								}
+								
+								
+
 								?>
 								<tr>
 									<th><?php echo $cont;?></th>
@@ -100,15 +137,31 @@ $cont = 0;
 									<td><?php echo $streak?></td>
 								</tr>
 								<?php
-								if ($streak > $largest_streak) {
-									$largest_streak = $streak;
-									$largest_holder = $holder;
-								}
+
 							}
 						}
 					}
+
+					/*
+					echo "<br>".$holder."...".$season."...".$mode;
+					if ($mode == 0) {
+						$aux = "playoffs";
+					}
+					else {
+						$aux = "regular";
+					}
+
+					foreach ($json_data as $key => $row) {
+	                    if ($row['season'] == $season && $row['type'] == $aux) {
+	                        $json_data[$key]['holder'] = $holder;
+	                    }
+	                }
+	                */
 					
 					if ($mode == 0) {
+						if (!$all) {
+							break;
+						}
 						$season++;
 					}
 					if ($season == 1948) {
@@ -118,25 +171,18 @@ $cont = 0;
 					if ($season > 1949) {
 						$league = "NBA";
 					}
-					fclose($file);
-					?>
 					
-					<tr>
-						<td colspan = 8><?php echo $largest_streak; ?> <= largest streak</td>					
-					</tr>
-					<tr>
-						<td colspan = 8><?php echo $largest_holder; ?> <= largest holder</td>
-					</tr>
-					<tr>
-						<td colspan = 8><?php echo $season; if ($mode == 1) {echo " Playoffs ";} ?> Game Log</td>
-					</tr>
-					<?php
+					fclose($file);
 				}
 				?>
 				</tbody>
 			</table>
 		</div>
-		<?php
-		?>
 	</body>
 </html>
+<?php
+/*
+$new_json_string = json_encode($json_data);
+file_put_contents("res/season-holders.json", $new_json_string);
+*/
+?>
